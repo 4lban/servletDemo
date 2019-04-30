3f8ba8aa3a8f6f465426d5ab9f91ce6e9c5d01e8
Alban Maxhuni <alban.maxhuni@gmail.com>
2019-04-30 17:40:08 +0200

    Build an MVC App with Servlets

---
+++ servletdemo/WebContent/view_students_two.jsp
@@ -0,0 +1,39 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<html>
+
+<body>
+<h2>Student Table Demo</h2>
+<hr>
+<br/>
+
+<table border="1">
+	<tr>
+		<th>First Name</th>
+		<th>Last Name</th>
+		<th>Email</th>
+	</tr>
+	<c:forEach var="tempStudent" items="${student_list}">
+		<tr>
+			<td>${tempStudent.firstName} </td>	<!-- will call tempStudent.getFirstName() -->
+			<td>${tempStudent.lastName} </td>
+			<td>${tempStudent.email} </td>
+		</tr>
+	</c:forEach>
+</table>
+
+</body>
+
+</html>
+
+
+
+<!-- 
+
+	Student Table Demo
+	---------------------------------------------------
+	 First Name  |	 Last Name  |	 Email
+	Mary	     |	Public	    |	mary@luv2code.com
+	John	     |  Doe	    |	john@luv2code.com
+	Ajay	     |	Rao	    |	ajay@luv2code.com
+ -->
---
+++ servletdemo/src/com/luv2code/servletdemo/MvcDemoServlet.java
@@ -0,0 +1,52 @@
+package com.luv2code.servletdemo;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MvcDemoServlet
+ */
+@WebServlet("/MvcDemoServlet")
+public class MvcDemoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MvcDemoServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Step 0: Add data
+		String[] students = {"Susan", "Anil", "Mohamed", "Trupti"};
+		request.setAttribute("student_list", students);
+		
+		// Step 1: get request dispatcher
+		//	make a call to a JSP page
+		RequestDispatcher dispatcher = 
+			request.getRequestDispatcher("/view_students.jsp");
+		
+		// Step 2: forward the request to JSP
+		// 	Sending data to specified JSP (/view_students.jsp)
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
